50d4dda4c2b96e89ac1b7d5373c35f280bf3eb4c
manalsolanki <97manal@gmail.com>
2021-08-16 00:24:59 -0400

    Initally commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Landing Page</h1>
+  <div>
+    <a href="qualityAsurance.html">Quality Assurance</a>
+    <a href="Security.html">Security</a>
+    <a href="badExample.html">Bad Example </a>
+  </div>
+`
